ff6b7b9b7e93b24629081bca8ced5bde5689881a
nfabri <fabri.nicolas@yahoo.fr>
2016-10-01 16:49:11 +0200

    init fosrestbundle

---
+++ src/NotSoFarAway/ApiBundle/Controller/ArticleController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace NotSoFarAway\ApiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController extends Controller
+{
+  public function getArticlesAction(Request $request)
+  {
+  }
+
+  public function getArticleAction(Request $request)
+  {
+  }
+}
